c33ccbaf3575fdf2468537d9e83ae1cd6b1b7eb9
maddonix <tlux14@googlemail.com>
2024-04-06 18:09:06 +0200

    swap file gpu client 04

--- profiles/agl-gc04/init-swap.nix
+++ profiles/agl-gc04/init-swap.nix
@@ -1,4 +1,4 @@
 {...}:
 {
-    boot.initrd.luks.devices."luks-64cc1caf-89ca-443f-88a9-05d10cd3a221".device = "/dev/disk/by-uuid/64cc1caf-89ca-443f-88a9-05d10cd3a221";
+    boot.initrd.luks.devices."luks-447d17e7-c451-4d1c-8731-9e42feaf5d24".device = "/dev/disk/by-uuid/447d17e7-c451-4d1c-8731-9e42feaf5d24";
 }

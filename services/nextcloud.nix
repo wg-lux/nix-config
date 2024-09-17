6b628f8f83605eae8234b989bc01dd3e1cdd0126
maddonix <tlux14@googlemail.com>
2024-09-17 16:40:20 +0200

    nextcloud test activate notify push

--- services/nextcloud.nix
+++ services/nextcloud.nix
@@ -63,7 +63,7 @@ in
 			
 			autoUpdateApps.enable = true;
 			enableImagemagick = true; 
-			notify_push.enable = false;
+			notify_push.enable = true;
 			notify_push.socketPath = "/run/nextcloud-notify_push/sock"; # default
 
 			configureRedis = true; # default is same as notify_push.enable

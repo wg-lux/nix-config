37e0207f98a657bc624adf33903acefe303d4abf
maddonix <tlux14@googlemail.com>
2024-10-02 01:53:12 +0200

    fx

--- profiles/shared/local-nginx.nix
+++ profiles/shared/local-nginx.nix
@@ -11,24 +11,24 @@ in
     networking.firewall.allowedTCPPorts = [ port ];
 
     services.nginx = {
-        defaultListen = [
-            { addr = "${config.networking.hostName }-intern.endo-reg.net"; port = port; }
-        ];
+        # defaultListen = [
+        #     { addr = "${config.networking.hostName }-intern.endo-reg.net"; port = port; }
+        # ];
 
         enable = true;
-        recommendedProxySettings = true;
-        recommendedOptimisation = true;
-        recommendedGzipSettings = true;
-        recommendedTlsSettings = true;
-        appendHttpConfig = ''
-            proxy_set_header Host $host;
-            proxy_set_header X-Forwarded-Host $host;
-            proxy_set_header X-Forwarded-Proto $scheme;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_ssl_server_name on;
-            proxy_pass_header Authorization;
-        '';
+        # recommendedProxySettings = true;
+        # recommendedOptimisation = true;
+        # recommendedGzipSettings = true;
+        # recommendedTlsSettings = true;
+        # appendHttpConfig = ''
+        #     proxy_set_header Host $host;
+        #     proxy_set_header X-Forwarded-Host $host;
+        #     proxy_set_header X-Forwarded-Proto $scheme;
+        #     proxy_set_header X-Real-IP $remote_addr;
+        #     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        #     proxy_ssl_server_name on;
+        #     proxy_pass_header Authorization;
+        # '';
 
         
 

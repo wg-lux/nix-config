8037f0b0d6196436606a4ed2e32d17432136969d
maddonix <tlux14@googlemail.com>
2024-10-01 22:24:58 +0200

    asd

--- profiles/base/hosts.nix
+++ profiles/base/hosts.nix
@@ -31,52 +31,52 @@ let
 in {
     networking.hosts = {
         # AGL S01
-        s01-ip = [
+        "${s01-ip}" = [
             client-domains."${s01}"
         ];
 
         # AGL S02
-        s02-ip = [
+        "${s02-ip}" = [
             client-domains."${s02}"
         ];
 
         # AGL S03
-        s03-ip = [
+        "${s03-ip}" = [
             client-domains."${s03}"
         ];
 
         # AGL S04
-        s04-ip = [
+        "${s04-ip}" = [
             client-domains."${s04}"
         ];
 
         # AGL GC DEV
-        gc-dev-ip = [
+        "${gc-dev-ip}" = [
             client-domains."${gc-dev}"
         ];
 
         # AGL GC 01
-        gc01-ip = [
+        "${gc01-ip}" = [
             client-domains."${gc01}"
         ];
 
         # AGL GC 02
-        gc02-ip = [
+        "${gc02-ip}"= [
             client-domains."${gc02}"
         ];
 
         # AGL GC 03
-        gc03-ip = [
+        "${gc03-ip}" = [
             client-domains."${gc03}"
         ];
 
         # AGL GC 04
-        gc04-ip = [
+        "${gc04-ip}" = [
             client-domains."${gc04}"
         ];
 
         # AGL GC 05
-        gc05-ip = [
+        "${gc05-ip}" = [
             client-domains."${gc05}"
         ];
     };
